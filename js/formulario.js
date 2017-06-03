0074a98b9eabcadc277054a90e991fa31f473832
Matheus Gamalier <matheusgamalier1@gmail.com>
2017-06-03 16:40:05 -0300

    Add files via upload

---
+++ js/formulario.js
@@ -0,0 +1,9 @@
+﻿function validacao() {
+    if (document.getElementById('nome').value == "" || document.getElementById('email').value == "" || document.getElementById('assunto').value == "" || document.getElementById('descricao').value == "")
+    {
+		alert("Preencha todos os campos.")
+        return false;
+    }
+    
+    return true;
+}
